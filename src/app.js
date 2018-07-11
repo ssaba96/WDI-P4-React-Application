b1f8f09b6d016da7dd86177efde7cb97c33d4c0d
Family <sabziamjad@gmail.com>
2018-07-11 13:14:57 +0100

    Added Carousel to Home page

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
 import AuthLogin from './components/auth/Login';
 import AuthRegister from './components/auth/Register';
+
+
+import 'bulma';
+import 'react-select/dist/react-select.css';
+import './scss/style.scss';
 
 class App extends React.Component {
   render() {
@@ -15,6 +21,7 @@ class App extends React.Component {
               <Switch>
                 <Route path="/login" component={AuthLogin} />
                 <Route path="/register" component={AuthRegister} />
+                <Route path="/" component={Home} />
               </Switch>
             </div>
           </section>
