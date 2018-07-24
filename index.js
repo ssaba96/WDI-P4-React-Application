f5011343e08d2a349cdb431ce98c68d74228b64e
Family <sabziamjad@gmail.com>
2018-07-24 16:33:49 +0100

    Adding static folder

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const { dbURI, port } = require('./config/environment');
 
 mongoose.connect(dbURI);
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use(bodyParser.json());
 app.use('/api', routes);
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
